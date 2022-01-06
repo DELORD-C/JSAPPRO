501236bacc2ba2caa66fe184686afd54d42a4855
Clément <clement.delord@epitech.eu>
2022-01-06 09:53:42 +0100

    strings

--- js/script.js
+++ js/script.js
@@ -18,5 +18,10 @@ Object.assign(target, source);
 //Object.assign(cible, source) permet de copier les attributs de l'objet source dans l'objet cible (remplace)
 
 let date = new Date;
-console.log(date);
+console.log(date.toLocaleDateString());
+console.log(('0' + date.getDate()).slice(-2) + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + date.getFullYear());
 //Gràce à l'object Date, afficher dans la console la date d'aujourd'hui au format : DD/MM/AAAA
+
+let str = 'Ceci est un test.';
+//Grâce aux méthodes de l'object String, modifier notre chaîne de caractères pour arriver au résultat suivant :
+//Ceci Est Un Exercice.
