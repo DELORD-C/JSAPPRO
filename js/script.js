0bd6bab410b5e40ee0762ae194471149c97308d6
Clément <clement.delord@epitech.eu>
2022-01-06 09:38:02 +0100

    object assign

--- js/script.js
+++ js/script.js
@@ -10,4 +10,9 @@ class Animal {
 }
 
 let chien = new Animal('Rex', 'Chien');
-chien.presentation();
+// chien.presentation();
+
+const target = { a: 1, b: 2 };
+const source = { b: 4, c: 5 };
+Object.assign(target, source);
+//Object.assign(cible, source) permet de copier les attributs de l'objet source dans l'objet cible (remplace)
