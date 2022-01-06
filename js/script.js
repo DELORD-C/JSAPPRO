2a419da975fbea3423a7809c9fabdf865ddb5bd5
Clément <clement.delord@epitech.eu>
2022-01-06 09:31:31 +0100

    déclaration et utilisation de méthode

--- js/script.js
+++ js/script.js
@@ -3,5 +3,11 @@ class Animal {
         this.nom = nom;
         this.type = type;
     }
+
+    presentation () {
+        console.log(this.nom + ' ' + this.type);
+    }
 }
 
+let chien = new Animal('Rex', 'Chien');
+chien.presentation();
