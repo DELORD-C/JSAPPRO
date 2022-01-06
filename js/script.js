e46bb587d7a2848b5d8c410948ffd6e80267d4c8
Clément <clement.delord@epitech.eu>
2022-01-06 10:08:20 +0100

    exercice string

--- js/script.js
+++ js/script.js
@@ -18,10 +18,20 @@ Object.assign(target, source);
 //Object.assign(cible, source) permet de copier les attributs de l'objet source dans l'objet cible (remplace)
 
 let date = new Date;
+//Gràce à l'object Date, afficher dans la console la date d'aujourd'hui au format : DD/MM/AAAA
 console.log(date.toLocaleDateString());
 console.log(('0' + date.getDate()).slice(-2) + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + date.getFullYear());
-//Gràce à l'object Date, afficher dans la console la date d'aujourd'hui au format : DD/MM/AAAA
 
 let str = 'Ceci est un test.';
 //Grâce aux méthodes de l'object String, modifier notre chaîne de caractères pour arriver au résultat suivant :
 //Ceci Est Un Exercice.
+
+let mots = str.split(' ');
+
+for (let index = 0; index < mots.length; index++) {
+    mots[index] = mots[index][0].toUpperCase() + mots[index].substring(1);
+}
+
+str = mots.join(' ');
+str = str.replace('Test', 'Exercice');
+
